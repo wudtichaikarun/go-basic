e8d7195187bdf6d0785baa06a09a21a546797b21
wudtichai <wudtichai.kar@ascendcorp.com>
2022-09-25 16:47:02 +0700

    pointer example

--- main.go
+++ main.go
@@ -3,5 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello , Romantic")
+	d := 2
+	double(&d)
+
+	// 4
+	fmt.Println(d)
+}
+
+/*
+var s string
+// <--- ประกาศ p เพื่อมาเก็บ address ของ string ตัวอื่น
+var p * string
+
+// <---- ref p to address ของ s
+p = &s
+
+// <---- assign value s ผ่านตัวแปร p
+*p = "hello world"
+*/
+
+func double(d *int) {
+	*d = *d * 2
 }
