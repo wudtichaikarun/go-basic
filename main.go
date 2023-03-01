4f64657326d32c3dd0693eb5cdb0902a54797fe6
Wudtichai Karun <wudtichai.kar@ascendcorp.com>
2023-03-01 21:16:05 +0700

    demo slice

--- main.go
+++ main.go
@@ -3,25 +3,64 @@ package main
 import "fmt"
 
 func main() {
-	d := 2
-	double(&d)
+	var s []int
 
-	// 4
-	fmt.Println(d)
-}
+	if s == nil {
+		fmt.Println("it's nil")
+		fmt.Println("++++++++++++++++++++++++++++++")
+	}
 
-/*
-var s string
-// <--- ประกาศ p เพื่อมาเก็บ address ของ string ตัวอื่น
-var p * string
+	// []int is type of s
+	// 4 is the length of the slice
+	// 6 is the capacity of the slice
+	s = make([]int, 4, 6)
+	// value of s is [0 0 0 0]
+	fmt.Println("emp:", s)
+	// value of len(s) is 4
+	fmt.Println("len:", len(s))
+	// value of cap(s) is 6
+	fmt.Println("cap(s)", cap(s))
+	fmt.Println("++++++++++++++++++++++++++++++")
 
-// <---- ref p to address ของ s
-p = &s
+	s[0] = 1
+	s[1] = 2
+	// value of s is [1 2 0 0]
+	fmt.Println("set:", s)
+	fmt.Println("++++++++++++++++++++++++++++++")
 
-// <---- assign value s ผ่านตัวแปร p
-*p = "hello world"
-*/
+	// append is a function that adds an element to the end of the slice
+	s = append(s, 10)
+	// after append the value of s is [1 2 0 0 10]
+	fmt.Println("apd:", s)
+	// value of len(s) is 5
+	fmt.Println("len:", len(s))
+	// value of cap(s) is 6
+	fmt.Println("cap(s)", cap(s))
+	fmt.Println("++++++++++++++++++++++++++++++")
 
-func double(d *int) {
-	*d = *d * 2
+	// a is array
+	// [...] is a array literal
+	a := [...]int{1, 3, 5, 7, 9}
+	// value of a is [1 3 5 7 9]
+	fmt.Println("a:", a)
+	// mutate the slice
+	s = a[:]
+	// a: [1 3 5 7 9]
+	// the pointer of s is the same as the pointer of a
+	// s:0xc000016120 is same as a:0xc000016120
+
+	// len:5 cap:5 s:0xc000016120 &a:0xc000016120
+	fmt.Printf("len:%d cap:%d s:%p &a:%p\n", len(s), cap(s), s, &a)
+	fmt.Println("++++++++++++++++++++++++++++++")
+
+	s = append(s, 11, 13)
+	// after append len and cap is more than maximum
+	// go create new array and copy the old array to new array
+	// now the pointer of s is changed to new array
+	// s:0xc000014190 different from a:0xc000016120
+
+	// len:7 cap:10 s:0xc000014190 &a:0xc000016120
+	// after append the value of s is [1 2 0 0 10 11 13]
+	fmt.Printf("len:%d cap:%d s:%p &a:%p\n", len(s), cap(s), s, &a)
+	fmt.Println("++++++++++++++++++++++++++++++")
 }
