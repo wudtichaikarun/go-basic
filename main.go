5ce68a7705179e9ba9dfcc5fb6f86d45678d7c6f
Wudtichai Karun <wudtichai.kar@ascendcorp.com>
2023-03-05 15:26:35 +0700

    feat condition if

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
 	fmt.Println("Hello , Romantic")
+
+	// เราสามารถใส่ short statement ก่อน condition
+	// n, err := strconv.Atoi("5s")
+	// if err != nil {
+	// 	fmt.Println("NaN:", err)
+	// } else {
+	// 	fmt.Println(n)
+	// }
+	if n, err := strconv.Atoi("5s"); err != nil {
+		fmt.Println("NaN:", err)
+	} else {
+		fmt.Println(n)
+	}
+
+}
+
+func showName(name string) {
+	if name != "" {
+		fmt.Println("Hello $s\n", name)
+	} else {
+		fmt.Println("Hello friend")
+	}
 }
