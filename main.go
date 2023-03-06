96b15272caac666ba05abd7a15216151eb0d13d9
Wudtichai Karun <wudtichai.kar@ascendcorp.com>
2023-03-06 14:57:25 +0700

    feat: waitgroup

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
+/*
+*
+ total time = 101.3965ms
+something
+something
+something
+101.3965ms
+*/
 func main() {
-	fmt.Println("Hello , Romantic")
+	wg := &sync.WaitGroup{}
+	wg.Add(3)
+
+	start := time.Now()
+
+	go doSomeThing(wg)
+	go doSomeThing(wg)
+	go doSomeThing(wg)
+
+	// ไม่ต้องเดา รอให้ทำงานเสร็จจริงๆ
+	wg.Wait()
+
+	fmt.Println(time.Since(start))
+}
+
+func doSomeThing(wg *sync.WaitGroup) {
+	defer wg.Done()
+	time.Sleep(100 * time.Millisecond)
+	fmt.Println("something")
 }
