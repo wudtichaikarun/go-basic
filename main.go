81ff86e2191895160dc287c01534f6062cd9c9f1
Wudtichai Karun <wudtichai.kar@ascendcorp.com>
2023-03-06 13:55:33 +0700

    feat: without goroutine

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
+	fmt.Println("start")
+	doSomeThing()
+	time.Sleep(time.Second)
 	fmt.Println("Hello , Romantic")
+}
+
+func doSomeThing() {
+	fmt.Println("do some thing")
 }
