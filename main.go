ff45d20460b41485ad551bbb2311cbf576dc8a8c
Wudtichai Karun <wudtichai.kar@ascendcorp.com>
2023-03-17 09:16:36 +0700

    feat: channel

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello , Romantic")
+	start := time.Now()
+
+	ch := make(chan int)
+	go fibonacci(ch)
+
+	for i := 0; i < 10; i++ {
+		value := <-ch
+		fmt.Println("ch value:", value, "at index", i)
+	}
+
+	fmt.Println("script took:", time.Since(start))
+}
+
+func fibonacci(ch chan int) {
+	a, b := 0, 1
+
+	time.Sleep(1 * time.Second)
+
+	for {
+		ch <- a
+		a, b = b, a+b
+	}
 }
